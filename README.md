ee8ec7269cf044c75f66e7b0d8c3b69f2ed2c0f1
alct <git@loconte.be>
2018-04-06 16:35:53 +0200

    Initial source files

---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# Export Tabs URLs
+
+[Export Tabs URLs](https://addons.mozilla.org/en-US/firefox/addon/export-tabs-urls-and-titles/) is a Web browser extension that allows to list and save all the opened tabs either to the clipboard or into a timestamped file.
+
+Among the existing extensions, I could not find one that was simple enough and/or had all the features that I needed, so I developed this one. It was also an excuse to play around with WebExtensions.
+
+Consider this add-on done (except for bugfixes). I may or may not add new features depending on the fun (or lack of it).
+
+![screenshot-1](https://imgs.be/5ac78166-e99.png)
+![screenshot-2](https://imgs.be/5ac7817f-8b.png)
+
+## License
+
+[GPLv3](LICENSE)
---
+++ popup/popup.js
@@ -0,0 +1,95 @@
+var d = document
+var w = window
+var counter, copybutton, exportbutton, checkbox, textarea
+
+// use proper namespace when run in Chrome
+if (typeof chrome === 'object') var browser = chrome
+
+w.addEventListener('load', function () {
+  counter = d.getElementsByClassName('counter')[0]
+  copybutton = d.getElementsByClassName('copy-button')[0]
+  exportbutton = d.getElementsByClassName('export-button')[0]
+  checkbox = d.getElementById('include-titles')
+  textarea = d.getElementById('urls')
+
+  checkbox.addEventListener('change', function () {
+    updatePopup()
+  })
+
+  copybutton.addEventListener('click', function () {
+    if (copybutton.classList.contains('disabled')) return
+
+    textarea.select()
+
+    var message = d.execCommand('copy') ? 'copiedToClipboard' : 'notCopiedToClipboard'
+
+    browser.notifications.create('ExportTabsURLs', {
+      'type': 'basic',
+      'title': browser.i18n.getMessage('appName'),
+      'iconUrl': '../img/icon.svg',
+      'message': browser.i18n.getMessage(message)
+    })
+
+    copybutton.classList.add('disabled')
+
+    setTimeout(function () {
+      browser.notifications.clear('ExportTabsURLs')
+      copybutton.classList.remove('disabled')
+    }, 3000)
+  })
+
+  exportbutton.addEventListener('click', function () {
+    download(textarea.value)
+  })
+
+  updatePopup()
+
+  localization()
+})
+
+function updatePopup () {
+  browser.tabs.query(
+    {},
+    function (tabs) {
+      var list = ''
+      var format = '{url}\r\n'
+      var actualNbTabs = 0
+      var totalNbTabs = tabs.length
+
+      if (checkbox.checked) format = '{title}\r\n{url}\r\n\r\n'
+
+      for (i = 0; i < totalNbTabs; i++) {
+        var uri = URI(tabs[i].url)
+        var protocol = uri.protocol()
+
+        if (protocol === 'http' || protocol === 'https') {
+          list += format.replace(/{title}/g, tabs[i].title)
+                        .replace(/{url}/g, tabs[i].url)
+          actualNbTabs += 1
+        }
+      }
+
+      textarea.value = list
+      counter.textContent = actualNbTabs
+    }
+  )
+}
+
+function localization () {
+  d.querySelectorAll('[data-i18n]').forEach((node) => {
+    node.textContent = browser.i18n.getMessage(node.dataset.i18n)
+  })
+}
+
+function download (list) {
+  var element = d.createElement('a')
+  element.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(list)
+  element.download = moment().format('YYYYMMDDTHHmmssZZ') + '_ExportTabsURLs.txt'
+  element.style.display = 'none'
+
+  d.body.appendChild(element)
+
+  element.click()
+
+  d.body.removeChild(element)
+}
